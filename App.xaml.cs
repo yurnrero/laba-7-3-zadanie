01542a990fd5bb79026b95d4426dc89a5b7e9f77
Admin <Admin@DESKTOP-7MCGF0R>
2024-10-30 13:30:57 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace laba_7_3_zadanie
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
